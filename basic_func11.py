c55a52862662a1ff31ae45e87b481449444518eb
Jamshid93 <hamzayevjf@gmail.com>
2022-10-25 02:18:35 +0500

    basic_func11 done

--- basic_func11.py
+++ basic_func11.py
@@ -1,3 +1,10 @@
 # Create a function called main.
+from tkinter.messagebox import RETRY
+
+
+def main(a):
 # Create function argument a.
+    a
 # Return the absolute value of a.
+    return abs(a)
+print(main(-8))
