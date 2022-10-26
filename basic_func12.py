9b2a019a20c7101ce63e54e50a6bf65404a51a0e
Jamshid93 <hamzayevjf@gmail.com>
2022-10-26 16:26:56 +0500

    basic math func12

--- basic_func12.py
+++ basic_func12.py
@@ -1,4 +1,8 @@
 # Create a function called main.
+def main(a):
 # Create function argument a.
+    a
 # Return the value of a.
+    return a
 # Round the result to 2 decimal places.
+print(main(round(6.453,2)))
