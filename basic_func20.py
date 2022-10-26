fb2b45893777e798740aa71c06f1e5f02ebc4784
Jamshid93 <hamzayevjf@gmail.com>
2022-10-26 17:08:54 +0500

    function 20 done

--- basic_func20.py
+++ basic_func20.py
@@ -1,3 +1,10 @@
 # Create a function called main.
+from tkinter.messagebox import RETRY
+
+
+def main(a,b):
 # Create function arguments a and b.
+    a,b
 # Return the multiplication of a and b.
+    return a*b
+print(main(5,4))
